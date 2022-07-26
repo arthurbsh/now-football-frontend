b76edf3f980cb9136d4c120020f301d96a655a49
Arthur Holanda <arthur.holanda@futurice.com>
2022-07-26 12:11:04 +0300

    Add missing teams
    Adds Wales, Australia and Costa Rica to their groups
    Fixes England flag
    Minor style changes

--- src/components/simpleGroup/SimpleGroup.tsx
+++ src/components/simpleGroup/SimpleGroup.tsx
@@ -5,42 +5,42 @@ interface Props {
   teams: { name: string; flag: string }[];
 }
 
-const SimpleGroup = (props: Props) => {
+const SimpleGroup = ({ name, teams }: Props) => {
   return (
     <div className="SimpleGroup">
       <div className="SimpleGroup_titleArea">
-        <span className="SimpleGroup_title">{`Group ${props.name}`}</span>
+        <span className="SimpleGroup_title">{`Group ${name}`}</span>
       </div>
       <div className="SimpleGroup_teamsArea">
         <div className="SimpleGroup_team">
-          <span className="SimpleGroup_teamName">{props.teams[0].name}</span>
+          <span className="SimpleGroup_teamName">{teams[0].name}</span>
           <img
             className="SimpleGroup_teamFlag"
-            src={props.teams[0].flag}
+            src={teams[0].flag}
             alt="flag"
           />
         </div>
         <div className="SimpleGroup_team">
-          <span className="SimpleGroup_teamName">{props.teams[1].name}</span>
+          <span className="SimpleGroup_teamName">{teams[1].name}</span>
           <img
             className="SimpleGroup_teamFlag"
-            src={props.teams[1].flag}
+            src={teams[1].flag}
             alt="flag"
           />
         </div>
         <div className="SimpleGroup_team">
-          <span className="SimpleGroup_teamName">{props.teams[2].name}</span>
+          <span className="SimpleGroup_teamName">{teams[2].name}</span>
           <img
             className="SimpleGroup_teamFlag"
-            src={props.teams[2].flag}
+            src={teams[2].flag}
             alt="flag"
           />
         </div>
         <div className="SimpleGroup_team">
-          <span className="SimpleGroup_teamName">{props.teams[3].name}</span>
+          <span className="SimpleGroup_teamName">{teams[3].name}</span>
           <img
             className="SimpleGroup_teamFlag"
-            src={props.teams[3].flag}
+            src={teams[3].flag}
             alt="flag"
           />
         </div>
